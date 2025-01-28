2b1434022ea945788be4ce6833d8df66ca4215f0
Bishwas-paudel <paudelbishwas885@gmail.com>
2025-01-28 11:25:43 +0545

    zx

--- Learnify/obj/Debug/net6.0/Learnify.AssemblyInfo.cs
+++ Learnify/obj/Debug/net6.0/Learnify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b42676219f6e503c881ee437ee02452d39d96c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0591d6d8f7d430c2ca474e8ebf23b7dad669d175")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
